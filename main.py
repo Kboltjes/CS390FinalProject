c41f7f7386196692edd02eeaa3c37aabdc6b3e66
Kade Boltjes <kboltjes@purdue.edu>
2021-11-09 14:39:52 -0500

    Made minor changes

--- main.py
+++ main.py
@@ -3,7 +3,31 @@ import gym
 import numpy as np
 import pandas as pd
 
+# An observation is the image that is fed into the dqn.
+OBSERVATION_WIDTH = 210
+OBSERVATION_HEIGHT = 160
+OBSERVATION_CHANNELS = 3
+OBSERVATION_SHAPE = (OBSERVATION_WIDTH, OBSERVATION_HEIGHT, OBSERVATION_CHANNELS)
 
+
+######################################################################################################
+#                                           Pipeline
+######################################################################################################
+def ProcessObservation(observation):
+    '''
+    Description:
+        Takes in an observation of shape OBSERVATION_SHAPE and outputs a processed observation
+    Returns:
+        The processed observation
+    '''
+
+    # TODO: implementation
+    return observation
+
+
+######################################################################################################
+#                                           Environment
+######################################################################################################
 def SetupEnvironment():
     '''
     Description:
@@ -12,6 +36,7 @@ def SetupEnvironment():
     Returns:
         The gym environment object
     '''
+
     env = gym.make('Assault-v0', render_mode='human')
     env.reset()
     return env
@@ -27,11 +52,23 @@ def RandomSampleEnvTest(env):
     Returns:
         None
     '''
+
     for _ in range(1000):
         env.render() # render each frame in a window
-        env.step(env.action_space.sample()) # take a random action
+        
+        observation, reward, done, info = env.step(env.action_space.sample()) # take a random action
+
+        # TODO: we can use ProcessObservation to apply preprocessing on the observations (images) before we run them through our dqn
+        processedObservation = ProcessObservation(observation)
+
+        if done:
+            print("Finished Running Algorithm")
+            break
 
 
+######################################################################################################
+#                                           Main
+######################################################################################################
 if __name__ == '__main__':
     env = SetupEnvironment()
 
